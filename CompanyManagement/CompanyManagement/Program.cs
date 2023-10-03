2048cba774b45ccb5fe090c7e300cd944621357c
brendanGiraudet <brendan.giraudet1@gmail.com>
2023-10-03 11:08:07 +0200

    #2 : first commit

---
+++ CompanyManagement/CompanyManagement/Program.cs
@@ -0,0 +1,21 @@
+using CompanyManagement;
+using Fluxor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add Fluxor
+builder.Services.AddFluxor(config =>
+{
+    config
+      .ScanAssemblies(typeof(Program).Assembly)
+      .UseReduxDevTools();
+
+});
+
+await builder.Build().RunAsync();
